714799b9e41b11c091bee155829bda18edef6c6e
Ayan Shahab <ayanshahab766@gmail.com>
2026-03-06 02:10:38 +0500

    Changed to Prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+});
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter })
+}
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ["query"] });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+export default prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
